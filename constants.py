b1b8490cb8b55abb1597e5a2efd5c8e2a484497c
xiaohou <xiyang.hou@interval.im>
2024-03-12 17:22:03 +0800

    - fix

--- constants.py
+++ constants.py
@@ -8,11 +8,11 @@ CONTROL_CANNY = 'diffusers/controlnet-canny-sdxl-1.0-small'
 CONTROL_DEPTH = 'diffusers/controlnet-depth-sdxl-1.0-small'
 
 # IMAGE
-IMAGE_ORIGIN = ''
+IMAGE_ORIGIN = 'https://static.interval.im/stable-ai/upload-e28a0f1c3ba9.jpg?imageView2/2/w/1024/h/1024'
 
 # Control Image
-IMAGE_CANNY = ''
-IMAGE_DEPTH = ''
+IMAGE_CANNY = 'https://static.interval.im/stable-ai/trans-1710235307422.png'
+IMAGE_DEPTH = 'https://static.interval.im/stable-ai/trans-1710235297686.png'
 
 # prompt
 
