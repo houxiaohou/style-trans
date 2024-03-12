7459c0b6cff43954094835be3b30f0b06f7fc980
xiaohou <xiyang.hou@interval.im>
2024-03-12 21:48:25 +0800

    - fix

--- portrait-style-1-5.py
+++ portrait-style-1-5.py
@@ -1,7 +1,7 @@
 """
 真人风格化
 
-实现方式 1.4（文生图，ControlNet + prompt + IP-Adapter-FaceID）：
+实现方式 1.5（文生图，prompt + IP-Adapter-FaceID）：
 """
 import time
 
@@ -16,9 +16,7 @@ from constants import BASE_MODEL, CONTROL_CANNY, CONTROL_DEPTH, IMAGE_CANNY, IMA
 
 def portrait_trans(
         origin_image: str,
-        control_image: str,
         prompt: str,
-        control_type: str,
         ip_adapter_scale: float = 0.7,
         prompt_2: str = None,
         negative: str = None,
@@ -28,15 +26,8 @@ def portrait_trans(
         subfolder="models/image_encoder",
         torch_dtype=torch.float16,
     )
-    controlnet = ControlNetModel.from_pretrained(
-        CONTROL_CANNY if control_type == 'canny' else CONTROL_DEPTH,
-        variant="fp16",
-        use_safetensors=True,
-        torch_dtype=torch.float16,
-    ).to('cuda')
     pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(
         BASE_MODEL,
-        controlnet=controlnet,
         variant="fp16",
         use_safetensors=True,
         torch_dtype=torch.float16,
@@ -54,32 +45,19 @@ def portrait_trans(
         prompt_2=prompt_2,
         negative_prompt=negative,
         ip_adapter_image=load_image(origin_image),
-        image=load_image(control_image),
         num_inference_steps=30,
         num_images_per_prompt=4,
     ).images
     for image in images:
         stamp = int(time.time() * 1000)
-        image.save(f'output/1_4/{control_type}/{ip_adapter_scale}-{stamp}.png')
+        image.save(f'output/1_5/{ip_adapter_scale}-{stamp}.png')
 
 
 if __name__ == '__main__':
     for i in [0.3, 0.4, 0.5, 0.6, 0.7]:
-        # canny
         portrait_trans(
             IMAGE_ORIGIN,
-            IMAGE_CANNY,
             PROMPT,
-            control_type='canny',
-            negative=NEGATIVE,
-            ip_adapter_scale=i
-        )
-        # depth
-        portrait_trans(
-            IMAGE_ORIGIN,
-            IMAGE_DEPTH,
-            PROMPT,
-            control_type='depth',
             negative=NEGATIVE,
             ip_adapter_scale=i
         )
