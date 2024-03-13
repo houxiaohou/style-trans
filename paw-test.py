b02cfbe089683cf459856823807849581922a1b8
xiaohou <xiyang.hou@interval.im>
2024-03-13 17:06:56 +0800

    - fix

--- paw-test.py
+++ paw-test.py
@@ -47,9 +47,9 @@ if __name__ == '__main__':
         {
             'name': 'Simple Vector Art',
             'folder': 'vector',
-            'prompt': 'Simple Vector Art, {prompt}, 2D flat, simple shapes, minimalistic, professional graphic, flat color, high contrast, Simple Vector Art',
+            'prompt': 'Simple Vector Art, {prompt}, 2D flat, simple shapes, minimalistic, professional graphic, flat color, high contrast, Simple Vector Art, 4k, masterpiece',
             'negative': 'ugly, deformed, noisy, blurry, low contrast, 3D, photo, realistic',
-            'strength': 0.75,
+            'strength': 0.5,
         }
         # ,
         # {
@@ -63,18 +63,18 @@ if __name__ == '__main__':
     origin_images = [
         'https://static.interval.im/interval/XYcyfMZBGeXRnAPm.jpeg',
         'https://static.interval.im/interval/b2i4yZQmEzDtxSs8.jpeg',
-        'https://static.interval.im/interval/D73yjc72hcBXQRza.jpeg',
-        'https://static.interval.im/interval/p73SM4FTRKmh53cc.jpeg',
-        'https://static.interval.im/interval/p73SM4FTRKmh53cc.jpeg',
-        'https://static.interval.im/interval/esc2cberweJmkBzS.jpeg',
-        'https://static.interval.im/interval/4Nsz4czmy8TWHA3b.jpeg',
-        'https://static.interval.im/interval/2FsDfyeApJ5rsnYy.jpeg',
-        'https://static.interval.im/interval/Pms4GWmRj5JkrnAd.jpeg',
-        'https://static.interval.im/interval/2hRN7tC7aBkinWEC.jpeg',
-        'https://static.interval.im/interval/pkhKm6zwkyh4hyyH.jpeg',
-        'https://static.interval.im/interval/YG3hNFCKMDYrizJE.jpeg',
-        'https://static.interval.im/interval/TjC4nRGJ7WtrEemp.jpeg',
-        'https://static.interval.im/interval/QmGWeYiTr4xFFXzE.jpeg',
+        # 'https://static.interval.im/interval/D73yjc72hcBXQRza.jpeg',
+        # 'https://static.interval.im/interval/p73SM4FTRKmh53cc.jpeg',
+        # 'https://static.interval.im/interval/p73SM4FTRKmh53cc.jpeg',
+        # 'https://static.interval.im/interval/esc2cberweJmkBzS.jpeg',
+        # 'https://static.interval.im/interval/4Nsz4czmy8TWHA3b.jpeg',
+        # 'https://static.interval.im/interval/2FsDfyeApJ5rsnYy.jpeg',
+        # 'https://static.interval.im/interval/Pms4GWmRj5JkrnAd.jpeg',
+        # 'https://static.interval.im/interval/2hRN7tC7aBkinWEC.jpeg',
+        # 'https://static.interval.im/interval/pkhKm6zwkyh4hyyH.jpeg',
+        # 'https://static.interval.im/interval/YG3hNFCKMDYrizJE.jpeg',
+        # 'https://static.interval.im/interval/TjC4nRGJ7WtrEemp.jpeg',
+        # 'https://static.interval.im/interval/QmGWeYiTr4xFFXzE.jpeg',
     ]
     for _style in styles:
         for _index, _image in enumerate(origin_images):
