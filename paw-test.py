8945d92b1da1358633f0199b717445c72cd2a2a7
xiaohou <xiyang.hou@interval.im>
2024-03-13 16:43:24 +0800

    - fix

--- paw-test.py
+++ paw-test.py
@@ -18,6 +18,8 @@ def trans(index: int, image: str, style: dict, prompt: str):
 
     pipeline: StableDiffusionXLImg2ImgPipeline = StableDiffusionXLImg2ImgPipeline.from_pretrained(
         'playgroundai/playground-v2.5-1024px-aesthetic',
+        variant="fp16",
+        use_safetensors=True,
         torch_dtype=torch.float16,
     ).to('cuda')
     images = pipeline(
